8652ed47382ce10420ede78d98141c4bd40dffb3
Christian Wylie <christianswylie@gmail.com>
2025-04-11 17:51:54 -0700

    wtf server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // or '0.0.0.0' if you're on Linux or want to access from another device
+    port: 5173, // optional: specify the port explicitly
+  },
 });
