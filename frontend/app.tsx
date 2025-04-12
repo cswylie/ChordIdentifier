f19e926d6801d8fc0782bc272aa9d6873a4b1a03
Christian Wylie <christianswylie@gmail.com>
2025-04-11 17:03:43 -0700

    renamed app

---
+++ frontend/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Chord Identifier</h1>
+      <p>Enter a list of notes to identify the chord.</p>
+    </div>
+  );
+};
+
+export default App;
+
+// This is a simple React component that serves as the main entry point for the application.
