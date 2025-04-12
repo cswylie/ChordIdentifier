dfa4e184bdaecea866f984f09f5ed7216b4f4b9e
TimMcCain03 <tm3mcc@gmail.com>
2025-04-11 18:31:59 -0700

    yup

--- frontend/src/components/ChordSearchBar.tsx
+++ frontend/src/components/ChordSearchBar.tsx
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/react';
 const ChordSearchBar: React.FC = () => {
   return (
     <Box background="tomato" width="100%" padding="4" color="white">
-      This is the Box
+      This is the onion
     </Box>
   );
 };
