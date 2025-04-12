6584d4d07431fc6bfccc827258073a02bb359bbe
Christian Wylie <christianswylie@gmail.com>
2025-04-11 18:27:53 -0700

    component example

---
+++ frontend/src/components/ChordSearchBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const ChordSearchBar: React.FC = () => {
+  return (
+    <Box background="tomato" width="100%" padding="4" color="white">
+      This is the Box
+    </Box>
+  );
+};
+
+export default ChordSearchBar;
