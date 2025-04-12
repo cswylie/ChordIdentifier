a4533ddcbc756e1e484e1d50cc9fa5acd1d3e615
Christian Wylie <christianswylie@gmail.com>
2025-04-11 18:18:56 -0700

    opens on start

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true, // opens the browser window automatically
     host: true, // or '0.0.0.0' if you're on Linux or want to access from another device
     port: 5173, // optional: specify the port explicitly
   },
